e400d0606a6102a1bebaa678a853447ca437f45b
HaipingXiaotong <847186328@qq.com>
2018-04-14 14:29:22 +0800

    更新

--- http/index.js
+++ http/index.js
@@ -2,6 +2,7 @@ const http = require('http')
 const crypto = require('crypto')
 const express = require('express')
 const app = express()
+const url = require('url')
 // 创建一个http服务
 /* 100 接收请求
 200 请求正常 并返回200 ok
@@ -26,17 +27,29 @@ var server = app.listen(3000, function () {
 http.createServer((req, res, next) => {
     /* 设置响应头和
     Content-Type http首部字段 */
-    if (req.url === '/no-Cache') {
+    let urls = url.parse(req.url, true)
+    console.log(urls.query)
+    if (urls.query.id && urls.query.id === '1') {
+        time++
+    }
+    console.log(urls)
+    if (urls.pathname === '/no-Cache') {
         res.setHeader('Cache-Control', 'no-cache')
         var val = "Cache-Control:" + time
         var etag = crypto
             .createHash('md5')
             .update(val)
             .digest('hex')
+        // 强ETag值 弱ETag值 加w/
         res.setHeader('ETag', etag)
+        if (req.headers['if-none-match'] == etag) {
+            res.writeHead(304)
+            res.end(val)
+            return
+        }
+        res.writeHead(200)
         res.end(val)
     }
-
 }).listen(3000, '127.0.0.1', () => {
     console.log('Server listen 127.0.0.1:3000')
 })
